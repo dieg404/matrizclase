b45f0f26b1b21c90e76c5edab00f8001b3941cd7
dieg404 <158373396+dieg404@users.noreply.github.com>
2024-05-22 18:08:19 -0500

    v1

--- matriz.py
+++ matriz.py
@@ -1,13 +1,64 @@
-matriz_Todo = [["Productos", "Productos 1", "Producto 2", "Producto 3", "Producto 4", "Producto 5", "Producto 6",],
-               ["Lunes", 1, 2, 3, 4, 5, 6,], 
-               ["Martes", 0, 0, 0, 0, 0, 0,], 
-               ["Miercoles", 0, 0, 0, 0, 0, 0,], 
-               ["Jueves", 0, 0, 0, 0, 0, 0,], 
-               ["Viernes", 0, 0, 0, 0, 0, 0,], 
-               ["Sabado", 0, 0, 0, 0, 0, 0,], 
+supermercado = [["Productos", ],
+               ["Lunes", ], 
+               ["Martes", ], 
+               ["Miercoles", ], 
+               ["Jueves", ], 
+               ["Viernes", ], 
+               ["Sabado", ], 
                ]
 
-for i in range(len(matriz_Todo)):
-    if matriz_Todo[i][0] == "Lunes":
-        for j in range (1, len(matriz_Todo[1])):
-            print("hola")
+
+
+
+#Funcion de bulce para agregar los productos que se vendio
+def productos():
+    global x
+    while x <= c:
+        p = input(f"Ingrese el producto número {x} que vendió: ") #p -> productos
+        supermercado[0].insert(1, p)
+        lunes()
+        martes()
+        miercoles()
+        jueves()
+        viernes()
+        sabado()
+        print("-" * 30)
+        
+
+        x += 1
+
+
+def lunes():
+    c1 = int(input(f"Ingrese la cantidad que vendio del producto el dia Lunes: "))
+    supermercado[1].insert(1, c1)
+
+def martes():
+    c1 = int(input(f"Ingrese la cantidad que vendio del producto el dia Martes: "))
+    supermercado[2].insert(1, c1)
+
+def miercoles():
+    c1 = int(input(f"Ingrese la cantidad que vendio del producto el dia Miercoles: "))
+    supermercado[3].insert(1, c1)
+
+def jueves():
+    c1 = int(input(f"Ingrese la cantidad que vendio del producto el dia Jueves: "))
+    supermercado[4].insert(1, c1)
+
+def viernes():
+    c1 = int(input(f"Ingrese la cantidad que vendio del producto el dia Viernes: "))
+    supermercado[5].insert(1, c1)
+
+def sabado():
+    c1 = int(input(f"Ingrese la cantidad que vendio del producto el dia Sabado: "))
+    supermercado[6].insert(1, c1)
+    
+    
+
+
+c = int(input("Ingrese la cantidad de productos que vendio: ")) #c -> cantidad
+x = 1
+productos()
+
+
+for i in supermercado:
+    print(i)
