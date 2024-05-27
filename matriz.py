495e96a74d13bba1d8623e77b7971611a724c411
dieg404 <158373396+dieg404@users.noreply.github.com>
2024-05-27 10:48:05 -0500

    v6

--- matriz.py
+++ matriz.py
@@ -1,12 +1,10 @@
 supermercado = [
-               ["Productos", "  Lunes  ", " Martes  ", "Miercoles", " Jueves  ", " Viernes ", " Sabado  "],
-               ["    |    ", "    |    ", "    |    ", "    |    ", "    |    ", "    |    ", "    |    "], 
-               ["    v    ", "    v    ", "    v    ", "    v    ", "    v    ", "    v    ", "    v    "], 
+               ["Productos", " Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"]
                ]
 
 #Funcion de bulce para agregar los productos que se vendio
 def productos():
-    global x, pf, p
+    global x, pf, p, sum
     while x <= c:
         p = input(f"Ingrese el producto número {x} que vendió: ") #p -> productos
         supermercado.append([p])
@@ -18,102 +16,59 @@ def productos():
         viernes()
         sabado()
         print("-" * 30)
-        recorrdio()
         x += 1
         pf += 1
 
 # Función para agregar cuantos productos se vendieron por día, Lunes.
 def lunes():
     c1 = int(input(f"Ingrese la cantidad que vendió del producto el día Lunes: "))
-    if c1 < 10:
-        supermercado[pf].insert(1, f"    {c1}    ")
-    elif c1 >= 10 and c1 < 100:
-        supermercado[pf].insert(1, f"    {c1}   ")
-    elif c1 >= 100 and c1 < 1000:
-        supermercado[pf].insert(1, f"    {c1}  ")
-    else:
-        print("Esto no debe salir.")
+    supermercado[pf].insert(1, c1) 
+    recorrdio() 
 
 # Función para agregar cuantos productos se vendieron por día, Martes.
 def martes():
     c1 = int(input(f"Ingrese la cantidad que vendió del producto el día Martes: "))
-    if c1 < 10:
-        supermercado[pf].insert(2, f"    {c1}    ")
-    elif c1 >= 10 and c1 < 100:
-        supermercado[pf].insert(2, f"    {c1}   ")
-    elif c1 >= 100 and c1 < 1000:
-        supermercado[pf].insert(2, f"    {c1}  ")
-    else:
-        print("Esto no debe salir.")
+    supermercado[pf].insert(2, c1)
+    recorrdio()
 
 # Función para agregar cuantos productos se vendieron por día, Miércoles.
 def miercoles():
     c1 = int(input(f"Ingrese la cantidad que vendió del producto el día Miércoles: "))
-    if c1 < 10:
-        supermercado[pf].insert(3, f"    {c1}    ")
-    elif c1 >= 10 and c1 < 100:
-        supermercado[pf].insert(3, f"    {c1}   ")
-    elif c1 >= 100 and c1 < 1000:
-        supermercado[pf].insert(3, f"    {c1}  ")
-    else:
-        print("Esto no debe salir.")
+    supermercado[pf].insert(3, c1)
 
 # Función para agregar cuantos productos se vendieron por día, Jueves.
 def jueves():
     c1 = int(input(f"Ingrese la cantidad que vendió del producto el día Jueves: "))
-    if c1 < 10:
-        supermercado[pf].insert(4, f"    {c1}    ")
-    elif c1 >= 10 and c1 < 100:
-        supermercado[pf].insert(4, f"    {c1}   ")
-    elif c1 >= 100 and c1 < 1000:
-        supermercado[pf].insert(4, f"    {c1}  ")
-    else:
-        print("Esto no debe salir.")
+    supermercado[pf].insert(4, c1)
 
 # Función para agregar cuantos productos se vendieron por día, Viernes.
 def viernes():
     c1 = int(input(f"Ingrese la cantidad que vendió del producto el día Viernes: "))
-    if c1 < 10:
-        supermercado[pf].insert(5, f"    {c1}    ")
-    elif c1 >= 10 and c1 < 100:
-        supermercado[pf].insert(5, f"    {c1}   ")
-    elif c1 >= 100 and c1 < 1000:
-        supermercado[pf].insert(5, f"    {c1}  ")
-    else:
-        print("Esto no debe salir.")
+    supermercado[pf].insert(5, c1)
 
 # Función para agregar cuantos productos se vendieron por día, Sábado.
 def sabado():
     c1 = int(input(f"Ingrese la cantidad que vendió del producto el día Sábado: "))
-    if c1 < 10:
-        supermercado[pf].insert(6, f"    {c1}    ")
-    elif c1 >= 10 and c1 < 100:
-        supermercado[pf].insert(6, f"    {c1}   ")
-    elif c1 >= 100 and c1 < 1000:
-        supermercado[pf].insert(6, f"    {c1}  ")
-    else:
-        print("Esto no debe salir.")
+    supermercado[pf].insert(6, c1)
 
 
-contador = 0
-listap = []
-njbjvkbvnkmbvkmb
 def recorrdio():
-    for i in range (len(supermercado)):
-        if supermercado[i][0] == p:
-            for j in i:
-                listap.append(j)
-
-
+    sum = 0
+    global p, pf
+    for i in range(1, len(supermercado)):
+        for j in range (1, len(supermercado [i])):
+            sum += supermercado[i][j]
+        print("Este producto se vednio", sum, "veces en la semana.")
+        
+            
 
 
 #Inicio codigo.
 c = int(input("Ingrese la cantidad de productos que vendio: ")) #c -> cantidad
 x = 1
-pf = 3 
+pf = 1 
 productos()
 
-print ("Su lista de numeros es ", listap)
  
 
 #Imprimir matriz
