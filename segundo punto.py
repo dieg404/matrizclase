ca003e9e043b41c9f4a06e18bbc4e24be0cab61b
dieg404 <158373396+dieg404@users.noreply.github.com>
2024-05-30 18:50:58 -0500

    v11

--- segundo punto.py
+++ segundo punto.py
@@ -1,13 +1,12 @@
 estudiantes = [
-              ["Estudiantes", "Examen 1", "Examen 2", "Tarea 1", "Tarea 2", "Proyecto"]
-              ] 
+    ["Estudiantes", "Examen 1", "Examen 2", "Tarea 1", "Tarea 2", "Proyecto"]
+]
 
-
-#Funcion de bucle para agregar los estudiantes y sus notas
+# Función de bucle para agregar los estudiantes y sus notas
 def notas():
     global x, pf, e
     while x <= c:
-        e = input(f"Ingrese el nombre del estudiante número {x}: ") #e -> estudiantes
+        e = input(f"Ingrese el nombre del estudiante número {x}: ")  # e -> estudiantes
         estudiantes.append([e])
 
         examen1()
@@ -16,15 +15,14 @@ def notas():
         tarea2()
         proyecto()
         print("-" * 30)
-        recorrdio()
+        recorrido()
         x += 1
         pf += 1
-        
 
 # Función para agregar la nota del examen 1
 def examen1():
     c1 = int(input(f"Ingrese la nota que obtuvo el estudiante en el examen 1: "))
-    estudiantes[pf].insert(1, c1) 
+    estudiantes[pf].insert(1, c1)
 
 # Función para agregar la nota del examen 2
 def examen2():
@@ -46,31 +44,40 @@ def proyecto():
     c1 = int(input(f"Ingrese la nota que obtuvo el estudiante en el proyecto: "))
     estudiantes[pf].insert(5, c1)
 
-
-def recorrdio():
+# Función para calcular el promedio de cada estudiante
+def recorrido():
     global p, pf
     for i in range(1, len(estudiantes)):
-        sum = 0
-        for j in range (1, len(estudiantes [i])):
-            sum += estudiantes[i][j]
-    promedioe.append(sum/5)
+        suma = 0
+        for j in range(1, len(estudiantes[i])):
+            suma += estudiantes[i][j]
+        promedioe.append(suma / 5)
 
-    
-    
-diariop =[0,0,0,0,0,0]  
-        
+# Inicialización de variables
 promedioe = []
 
-#Inicio codigo.
-c = int(input("Ingrese la cantidad de estudiantes: ")) #c -> cantidad
+# Inicio del código
+c = int(input("Ingrese la cantidad de estudiantes: "))  # c -> cantidad
 x = 1
-pf = 1 
+pf = 1
 notas()
 
-#Imprimir matriz
+# Imprimir matriz
 for i in estudiantes:
     print(i)
 print("El promedio de cada estudiante es: ", promedioe)
 
+# Encontrar el estudiante con el promedio más alto y más bajo
+promediom = max(promedioe)
+promediomin = min(promedioe)
+indicem = promedioe.index(promediom)
+indicemin = promedioe.index(promediomin)
 
-#version 9
+promedio_e_max = estudiantes[indicem + 1][0]
+promedio_e_min = estudiantes[indicemin + 1][0]
+
+print(f"El estudiante con el promedio más alto es: {promedio_e_max} con un promedio de {promediom}")
+print(f"El estudiante con el promedio más bajo es: {promedio_e_min} con un promedio de {promediomin}")
+
+
+#version 11
